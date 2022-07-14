b13b2a1e479678aa3eb5d864044f1fd217cafa21
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2022-07-14 15:33:11 +0545

    created super user

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("hello developers")
-
-def room(request):
-    return HttpResponse("I am in room")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
